21fb121f56da1da85bd448eaa964e6c1bef4d17a
Chris Wynne <chris.wynndow@gmail.com>
2015-11-20 13:53:13 +0000

    Adds context groups to the spec file and improves random tests for 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,5 +2,6 @@ def fizzbuzz(num)
   output = ''
   output << 'fizz' if num % 3 == 0
   output << 'buzz' if num % 5 == 0
-  output == '' ? num : output
+  output == '' ?  num : output
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,28 +2,35 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
 
-  it 'when passed the number 3 returns fizz' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  context 'when handed a single number :' do
+    it 'returns fizz when passed 3' do
+      expect(fizzbuzz(3)).to eq 'fizz'
+    end
+
+    it 'returns buzz when passed 5' do
+      expect(fizzbuzz(5)).to eq 'buzz'
+    end
+
+    it 'returns the original number of not divisable by 3 or 5' do
+      expect(fizzbuzz(17)).to eq 17
+    end
   end
 
-  it 'returns buzz when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+  context 'when handed a multiple: ' do
+
+    it 'returns fizz when passed a multiple of 3' do
+      expect(fizzbuzz((0..1000).reject{|num| num % 5 == 0}.select{|num| num % 3 == 0}.sample)).to eq 'fizz'
+    end
+
+    it 'returns buzz when passed a multiple of 5' do
+      expect(fizzbuzz((0..1000).reject{|num| num % 3 == 0}.select{|num| num % 5 == 0}.sample)).to eq 'buzz'
+    end
+
+    it 'returns fizzbuzz when passed a multiple of 3 AND 5' do
+      expect(fizzbuzz(rand(1000)*3*5)).to eq 'fizzbuzz'
+    end
+
   end
 
-  it 'returns fizz when passed a multiple of 3' do
-    expect(fizzbuzz(rand(100)*3)).to eq 'fizz'
-  end
-
-  it 'returns buzz when passed a multiple of 5' do
-    expect(fizzbuzz(rand(100)*5)).to eq 'buzz'
-  end
-
-  it 'returns fizzbuzz when passed a multiple of 3 AND 5' do
-    expect(fizzbuzz(rand(100)*3*5)).to eq 'fizzbuzz'
-  end
-
-  it 'returns the original number of not divisable by 3 or 5' do
-    expect(fizzbuzz(17)).to eq 17
-  end
 
 end
