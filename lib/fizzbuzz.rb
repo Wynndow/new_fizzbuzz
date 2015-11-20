9c8deab05523445edd01c406ab16cefa80b3950f
Chris Wynne <chris.wynndow@gmail.com>
2015-11-20 13:33:56 +0000

    Adds a couple of test cases for multiple's of 3 & 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,4 @@
 def fizzbuzz(num)
-  return 'fizz' if num == 3
-  return 'buzz' if num == 5
+  return 'fizz' if num % 3 == 0
+  return 'buzz' if num % 5 == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,12 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
+  it 'returns fizz when passed a multiple of 3' do
+    expect(fizzbuzz(rand(100)*3)).to eq 'fizz'
+  end
+
+  it 'returns buzz when passed a multiple of 5' do
+    expect(fizzbuzz(rand(100)*5)).to eq 'buzz'
+  end
+
 end
